6741acd0f0d6f7461095dc912636e89bc2aae4ab
Preston Rodeniser <preston.rodeniser@rolemodelsoftware.com>
2019-07-11 15:18:51 -0400

    Update readme template naming

--- lib/generators/rolemodel/readme/readme_generator.rb
+++ lib/generators/rolemodel/readme/readme_generator.rb
@@ -3,7 +3,8 @@ module Rolemodel
     source_root File.expand_path('templates', __dir__)
 
     def install_readme
-      @project_name = ask "What is the project name? Example: 'project_name':", :yellow
+      @project_name = Rails.application.class.parent_name
+      @project = @project_name.underscore
       template 'README.md.erb', 'README.md'
     end
   end
