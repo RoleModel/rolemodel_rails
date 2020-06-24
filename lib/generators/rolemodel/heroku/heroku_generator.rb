d9891e33bcc3ac73ddd5c4eff56c8afad11b03b4
Kyle Smith <kyle.smith@rolemodelsoftware.com>
2020-06-24 11:04:53 -0500

    Remove unnecessary bin/heroku_release

--- lib/generators/rolemodel/heroku/heroku_generator.rb
+++ lib/generators/rolemodel/heroku/heroku_generator.rb
@@ -2,12 +2,6 @@ module Rolemodel
   class HerokuGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
-    def install_bin_heroku_release
-      template 'bin/heroku_release'
-      # -rwxr-xr-x
-      chmod 'bin/heroku_release', 0755 & ~File.umask, verbose: false
-    end
-
     def install_app_json
       @project_name = Rails.application.class.parent_name
       template 'app.json.erb', 'app.json'
