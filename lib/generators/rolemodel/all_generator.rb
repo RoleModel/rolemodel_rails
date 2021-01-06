7051e8e632f9fdb29137a5bc69f84e8a0bf2a6c9
Kyle Smith <kyle.smith@rolemodelsoftware.com>
2021-01-06 10:41:07 -0600

    Run icons generator after Webpacker generator

--- lib/generators/rolemodel/all_generator.rb
+++ lib/generators/rolemodel/all_generator.rb
@@ -3,11 +3,11 @@ module Rolemodel
     source_root File.expand_path('templates', __dir__)
 
     def run_all_the_generators
-      generate 'rolemodel:css:all'
       generate 'rolemodel:github'
       generate 'rolemodel:heroku'
       generate 'rolemodel:readme'
       generate 'rolemodel:webpacker'
+      generate 'rolemodel:css:all'
       generate 'rolemodel:testing:all'
       generate 'rolemodel:soft_destroyable'
       generate 'rolemodel:saas:all'
