0ad3c9407552482cfc7e3ca1388e10afa7c10f21
Tim Irwin <tim.irwin@rolemodelsoftware.com>
2021-01-11 16:27:45 -0600

    Update per PR comments

--- lib/generators/rolemodel/simple_form/templates/config/initializers/simple_form.rb
+++ lib/generators/rolemodel/simple_form/templates/config/initializers/simple_form.rb
@@ -155,7 +155,7 @@ SimpleForm.setup do |config| # rubocop:disable Metrics/BlockLength
   # config.country_priority = nil
 
   # When false, do not use translations for labels.
-  config.translate_labels = false
+  # config.translate_labels = true
 
   # Automatically discover new inputs in Rails' autoload path.
   # config.inputs_discovery = true
