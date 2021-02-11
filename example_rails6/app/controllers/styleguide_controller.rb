7d0bee4e1e6d2d564bb75eb562ffe993ab0635e7
Josh McLeod <josh@mcleodfamily.org>
2021-02-11 13:57:05 -0500

    Tweak form styles

--- example_rails6/app/controllers/styleguide_controller.rb
+++ example_rails6/app/controllers/styleguide_controller.rb
@@ -2,4 +2,5 @@ class StyleguideController < ApplicationController
   def index; end
 
   def demo_form; end
+  def demo_form_prank; end
 end
--- example_rails6/config/routes.rb
+++ example_rails6/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/styleguide', to: 'styleguide#index'
   get '/demo-form', to: 'styleguide#demo_form'
+  get '/demo-form-prank', to: 'styleguide#demo_form_prank'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
