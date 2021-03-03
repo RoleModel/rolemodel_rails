0feda747f0e0cc30475340129574d661c920958b
Mark Kraemer <megamarker@gmail.com>
2021-03-03 17:10:16 -0600

    Fix flash and rails name

--- lib/generators/rolemodel/heroku/heroku_generator.rb
+++ lib/generators/rolemodel/heroku/heroku_generator.rb
@@ -3,7 +3,7 @@ module Rolemodel
     source_root File.expand_path('templates', __dir__)
 
     def install_app_json
-      @project_name = Rails.application.class.parent_name
+      @project_name = Rails.application.class.try(:parent_name) || Rails.application.class.module_parent_name
       template 'app.json.erb', 'app.json'
     end
 
