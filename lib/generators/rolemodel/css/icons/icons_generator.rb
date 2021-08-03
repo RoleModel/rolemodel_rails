8768ea0c1c95345ed5cfea8f1df62a28faa0f176
Josh McLeod <josh@mcleodfamily.org>
2021-08-03 09:42:31 -0400

    First pass at adding custom icons to icon generator

--- lib/generators/rolemodel/css/icons/icons_generator.rb
+++ lib/generators/rolemodel/css/icons/icons_generator.rb
@@ -3,17 +3,62 @@ module Rolemodel
     class IconsGenerator < Rails::Generators::Base
       source_root File.expand_path('templates', __dir__)
 
-      def add_material_icons
+      def install_webpack
+        generate 'rolemodel:webpacker' # unless File.exists?(Rails.root.join('app/javascript/packs/application.js'))
+        generate 'rolemodel:css:base' # unless File.exists?(Rails.root.join('app/javascript/packs/stylesheet.js'))
+      end
+
+      def install_packages
+        run 'yarn add svgo'
         run 'yarn add material-icons'
+        run 'yarn install'
       end
 
       def add_view_helper
         copy_file 'app/helpers/icon_helper.rb'
       end
 
+      def add_webpack_loaders
+        copy_file 'config/webpack/loaders/custom-icon-loader.js'
+        copy_file 'config/webpack/loaders/svg-color-override-loader.js'
+      end
+
       def add_css
         copy_file 'app/javascript/stylesheets/components/icon.scss'
         append_file 'app/javascript/packs/stylesheets.scss', "@import 'stylesheets/components/icon';"
+      end
+
+      def add_images_directory
+        run 'mkdir app/javascript/images'
+      end
+
+      def modify_webpack_environment
+        inject_into_file 'config/webpack/environment.js', after: "const { environment } = require('@rails/webpacker')\n" do
+          <<~'JS'
+
+            environment.loaders.insert(
+              'customIcons',
+              {
+                test: /\.svg$/,
+                use: [{
+                  loader: require.resolve('./loaders/custom-icon-loader')
+                }],
+              },
+              { before: 'file' }
+            )
+          JS
+        end
+      end
+
+      def modify_application
+
+        inject_into_file 'app/javascript/packs/application.js', after: "ActiveStorage.start()\n" do
+          <<~'JS'
+
+            // require custom icons
+            require.context("images")
+          JS
+        end
       end
     end
   end
--- lib/generators/rolemodel/css/icons/templates/app/helpers/icon_helper.rb
+++ lib/generators/rolemodel/css/icons/templates/app/helpers/icon_helper.rb
@@ -1,5 +1,42 @@
 module IconHelper
-  def icon(name, classes: nil, color: nil, hover_text: nil)
-    tag.span(name, class: "material-icons #{classes}", style: "color: var(--color-#{color})", title: hover_text)
+  def icon(name, classes: nil, color: nil, hover_text: name)
+    contents = custom_icon(name) || name
+
+    tag.span(contents, class: icon_classes(classes, custom_icon(name)), style: "color: var(--color-#{color})", title: hover_text)
+  end
+
+  private
+
+  def icon_classes(classes, is_custom)
+    icon_class = is_custom ? 'custom-icons' : 'material-icons'
+
+    classes.present? ? classes.prepend("#{icon_class} ") : icon_class
+  end
+
+  def custom_icon(name)
+    # Check for a custom SVG icon matching the given name.
+    custom_icon_path = Webpacker.manifest.lookup("media/images/#{name}.svg")
+    return if custom_icon_path.blank?
+
+    custom_icon_contents = load_svg_from_file_or_dev_server(custom_icon_path)
+
+    # These SVG files can safely be marked html_safe since we created them and
+    # they are part of this app's code.
+    custom_icon_contents.html_safe # rubocop:disable Rails/OutputSafety
+  end
+
+  def load_svg_from_file_or_dev_server(custom_icon_path)
+    # Read in SVG file. Needs special handling for the webpack dev server
+    # because that doesn't write out any of the files.
+    if Webpacker.dev_server.running?
+      dev_server = Webpacker.dev_server
+      custom_icon_path.slice!("#{dev_server.protocol}://#{dev_server.host_with_port}")
+      connection = URI.open("#{dev_server.protocol}://#{dev_server.host_with_port}#{custom_icon_path}")
+      contents = connection.read
+      connection.close
+      contents
+    else
+      File.read(File.join(Webpacker.config.public_path, custom_icon_path))
+    end
   end
 end
