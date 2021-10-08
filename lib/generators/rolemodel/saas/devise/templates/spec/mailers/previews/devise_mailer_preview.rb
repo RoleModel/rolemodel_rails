45f65c461820a022bac073825b62decdbf8c4c7a
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2021-10-08 17:34:20 -0400

    Add invitable email preview and protect devise previews based on what is enabled in the system

--- lib/generators/rolemodel/saas/devise/templates/spec/mailers/previews/devise_mailer_preview.rb
+++ lib/generators/rolemodel/saas/devise/templates/spec/mailers/previews/devise_mailer_preview.rb
@@ -1,22 +1,35 @@
 # mailers/previews/devise_mailer_preview.rb
 class DeviseMailerPreview < ActionMailer::Preview
-  def confirmation_instructions
-    Devise::Mailer.confirmation_instructions(User.first, 'faketoken')
-  end
 
   def email_changed
     Devise::Mailer.email_changed(User.first)
   end
 
-  def password_change
-    Devise::Mailer.password_change(User.first)
+  if Devise.mappings[:user].confirmable?
+    def confirmation_instructions
+      Devise::Mailer.confirmation_instructions(User.first, 'faketoken')
+    end
   end
 
-  def reset_password_instructions
-    Devise::Mailer.reset_password_instructions(User.first, 'faketoken')
+  if Devise.mappings[:user].recoverable?
+    def password_change
+      Devise::Mailer.password_change(User.first)
+    end
+
+    def reset_password_instructions
+      Devise::Mailer.reset_password_instructions(User.first, 'faketoken')
+    end
   end
 
-  def unlock_instructions
-    Devise::Mailer.unlock_instructions(User.first, 'faketoken')
+  if Devise.mappings[:user].lockable?
+    def unlock_instructions
+      Devise::Mailer.unlock_instructions(User.first, 'faketoken')
+    end
+  end
+
+  if Devise.mappings[:user].invitable?
+    def invitation_instructions
+      Devise::Mailer.invitation_instructions(User.first, 'faketoken')
+    end
   end
 end
