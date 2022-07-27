a5d6794cfcd3697bf974714dda77dc983fe02789
Stephen Finch <smfinch99@gmail.com>
2022-07-27 17:59:49 -0400

    review semaphore commands with Wes 🤙

--- lib/generators/rolemodel/semaphore/semaphore_generator.rb
+++ lib/generators/rolemodel/semaphore/semaphore_generator.rb
@@ -6,14 +6,16 @@ module Rolemodel
       @project_name = Rails.application.class.try(:parent_name) || Rails.application.class.module_parent_name
       template 'semaphore.yml.erb', '.semaphore/semaphore.yml'
 
-      if yes?('Does your project have JavaScript tests?')
+      if yes?('Does your project have JavaScript tests and/or eslint?')
         uncomment_lines('.semaphore/semaphore.yml', '- yarn test')
         uncomment_lines('.semaphore/semaphore.yml', '- yarn run eslint')
       end
     end
 
     def create_deplyment_commands
-      @project_name = Rails.application.class.try(:parent_name) || Rails.application.class.module_parent_name
+      default_heroku_prefix = (Rails.application.class.try(:parent_name) || Rails.application.class.module_parent_name).underscore.dasherize
+
+      @heroku_prefix = ask('Enter the heroku project prefix', default: default_heroku_prefix)
 
       template 'heroku-deployment-commands.sh', '.semaphore/heroku-deployment-commands.sh'
       template 'staging-deploy.yml.erb', '.semaphore/staging-deploy.yml'
