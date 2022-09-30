d4dbf2d3d4ff59314dec92f1fc44866b8eafc9dc
viacheslavmarkin <viacheslav.markin@rolemodelsoftware.com>
2022-09-30 14:07:53 -0400

    Revert running order of generators.

--- lib/generators/rolemodel/all_generator.rb
+++ lib/generators/rolemodel/all_generator.rb
@@ -3,19 +3,19 @@ module Rolemodel
     source_root File.expand_path('templates', __dir__)
 
     def run_all_the_generators
-      generate 'rolemodel:css:all'
       generate 'rolemodel:github'
-      generate 'rolemodel:heroku'
-      generate 'rolemodel:linters:all'
-      generate 'rolemodel:mailers'
-      generate 'rolemodel:readme'
-      generate 'rolemodel:saas:all'
       generate 'rolemodel:semaphore'
+      generate 'rolemodel:heroku'
+      generate 'rolemodel:readme'
+      generate 'rolemodel:webpacker'
+      generate 'rolemodel:css:all'
+      generate 'rolemodel:testing:all'
       generate 'rolemodel:simple_form'
       generate 'rolemodel:soft_destroyable'
+      generate 'rolemodel:saas:all'
+      generate 'rolemodel:linters:all'
+      generate 'rolemodel:mailers'
       generate 'rolemodel:source_map'
-      generate 'rolemodel:testing:all'
-      generate 'rolemodel:webpacker'
     end
   end
 end
