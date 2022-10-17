07f865ee1c0963b31bd3ec5a10aa880c9c32c99c
Ben Einwechter <ben1386@gmail.com>
2022-10-17 11:01:52 -0400

    Update rolemodel_rails bundler version

--- spec/rolemodel_rails_spec.rb
+++ spec/rolemodel_rails_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe RolemodelRails do
   it "has a version number" do
     expect(RolemodelRails::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
