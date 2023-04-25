f071e968c039811342af5e821c959945595ea00b
Andy Cohen <outlawandy@gmail.com>
2023-04-24 21:12:53 -0500

    inject or import optics modal/panel stylesheets

--- lib/generators/rolemodel/modals/modals_generator.rb
+++ lib/generators/rolemodel/modals/modals_generator.rb
@@ -2,11 +2,15 @@ module Rolemodel
   class ModalsGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
+    STYLESHEET_ROOT = 'app/javascript/stylesheets'.freeze
+    STYLESHEET_ENTRYPOINT = 'app/javascript/packs/stylesheets.scss'.freeze
+    OPTICS_MODAL_STYLE_PATH = 'https://raw.githubusercontent.com/RoleModel/optics/main/src/addons/rails-modal-and-panel/modal.scss'.freeze
+    OPTICS_PANEL_STYLE_PATH = 'https://raw.githubusercontent.com/RoleModel/optics/main/src/addons/rails-modal-and-panel/panel.scss'.freeze
+
     def install_turbo_confirm
-      # TODO: How should we handle this? - package name is: `@rolemodel/turbo-confirm`
+      run 'yarn add @rolemodel/turbo-confirm'
     end
 
-    # TODO: does this still belong here?
     def install_icons
       generate 'rolemodel:css:base' unless File.exists?(Rails.root.join('app/javascript/packs/stylesheets.scss'))
       generate 'rolemodel:css:icons'
@@ -17,22 +21,21 @@ module Rolemodel
       addendum = File.read([source_paths.last, '/application_addendum.js'].join)
       append_to_file 'app/javascript/packs/application.js', addendum
       copy_file 'app/javascript/controllers/toggle_controller.js'
-      copy_file 'app/javascript/helpers/loadingErrorTemplate.js' # TODO: is this still needed?
       copy_file 'app/views/layouts/application.html.slim'
       copy_file 'app/views/layouts/modal.html.slim'
       copy_file 'app/views/layouts/panel.html.slim'
       copy_file 'app/views/shared/_confirm.html.slim'
     end
 
-    # TODO: Optics?
     def add_css
-      copy_file 'app/javascript/stylesheets/components/panel.scss'
-      copy_file 'app/javascript/stylesheets/components/modal.scss'
-      inject_into_file 'app/javascript/packs/stylesheets.scss', "@import 'stylesheets/components/panel';\n", before: "@import 'stylesheets/styleguide'"
-      inject_into_file 'app/javascript/packs/stylesheets.scss', "@import 'stylesheets/components/modal';\n\n", before: "@import 'stylesheets/styleguide'"
+      if yes?('Are you using Optics?')
+        import_optics_addons
+      else
+        inject_optics_stylesheets
+      end
 
-      inject_into_file 'app/javascript/stylesheets/variables.scss', before: "  // Spacing" do
-        optimize_indentation <<~'VARIABLES', 2
+      inject_into_file "#{STYLESHEET_ROOT}/variables/tokens.scss", before: "  // Spacing" do
+        optimize_indentation <<~'SCSS', 2
           // Panel
           --panel-width: 40%;
           --panel-transition-speed: 400ms;
@@ -41,24 +44,54 @@ module Rolemodel
           --modal-width: 564px;
           --modal-transition-speed: 300ms;
 
-        VARIABLES
+        SCSS
       end
     end
 
-    # TODO: Optics?
     def add_to_styleguide
       inject_into_file 'app/controllers/styleguide_controller.rb', after: "def index; end\n" do
-        optimize_indentation <<~'VARIABLES', 2
+        optimize_indentation <<~'RUBY', 2
 
-          def full
-            render layout: 'modal'
+          def popup
+            render layout: params[:type]
           end
-        VARIABLES
+        RUBY
       end
-      route "get '/styleguide/full', to: 'styleguide#full', as: :styleguide_full"
-      copy_file 'app/views/styleguide/full.html.slim'
+
+      route "get '/styleguide/popup/:type', to: 'styleguide#popup', as: :styleguide_popup"
+      copy_file 'app/views/styleguide/popup_content.html.slim'
       copy_file 'app/views/styleguide/_modals.html.slim'
       append_to_file 'app/views/styleguide/index.html.slim', "  = render 'modals'"
+    end
+
+    private
+
+    def import_optics_addons
+      inject_into_file STYLESHEET_ENTRYPOINT, after: "@import '@rolemodel/optics/dist/scss/optics';" do
+        optimize_indentation <<~'SCSS', 2
+          @import '@rolemodel/optics/dist/scss/addons/rails-modal-and-panel/modal';
+          @import '@rolemodel/optics/dist/scss/addons/rails-modal-and-panel/panel';
+        SCSS
+      end
+    end
+
+    def inject_optics_stylesheets
+      get OPTICS_MODAL_STYLE_PATH do |modal_styles|
+        create_file "#{STYLESHEET_ROOT}/components/modal.scss" do
+          modal_styles
+        end
+      end
+
+      get OPTICS_PANEL_STYLE_PATH do |panel_styles|
+        create_file "#{STYLESHEET_ROOT}/components/panel.scss" do
+          panel_styles
+        end
+      end
+
+      append_to_file STYLESHEET_ENTRYPOINT, <<~SCSS
+        @import 'stylesheets/components/modal.scss';
+        @import 'stylesheets/components/panel.scss';
+      SCSS
     end
   end
 end
