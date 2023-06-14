9f99d61c53b8680ff4a3074fc1a146b21946d345
Andy Cohen <outlawandy@gmail.com>
2023-06-14 16:15:55 -0500

    update for Rails 7

--- lib/generators/rolemodel/modals/README.md
+++ lib/generators/rolemodel/modals/README.md
@@ -6,32 +6,86 @@
 * Modal and panel styling ..via `@rolemodel/optics` integration
 * Modal and panel layouts w/ link helpers (or you could just, e.g. `link_to ... data: {turbo_frame: 'modal'}`)
 * frame-missing handler (required for turbo-rails v1.4.0 and above)
+* a very simple stimulus controller `toggle` to help with modal/panel animations.
 
-Adds UI and JavaScript for opening modals and the panel within a Rails view
+## Turbo confirm example
 
-## Example delete confirmation link
-
-```ruby
-  = button_to "Test Confirm", model, method: :delete, class: "btn btn--primary", data: { \
-    turbo_confirm: "Are you sure you want to delete #{model.name}?",
-    confirm_details: "This action cannot be undone.",
-    confirm_button: "Make it so!",
+```slim
+  = button_to "Test Confirm", model, method: :delete, data: { \
+    turbo_confirm: "For real?!?",
+    confirm_details: "You're about to delete #{model.name}, forever. 😱",
+    confirm_button: "YOLO!",
   }
 ```
 
-data attributes other than `turbo-confirm` are optional.  See [turbo-confirm](https://github.com/RoleModel/turbo-confirm) for details.
+data attributes other than `turbo-confirm` are optional.  See [turbo-confirm](https://github.com/RoleModel/turbo-confirm) for more details.
 
-## Example Panel and Modal
+## Modal & Panel examples
 
-Modal: `= link_to "Modal Test", action_which_responds_with_modal_layout_path, class: "btn btn--primary", data: { turbo_frame: "modal" }`
-Panel: `= link_to "Panel Test", action_which_responds_with_panel_layout_path, class: "btn btn--primary", data: { turbo_frame: "panel" }`
+Modal: (the following 2 examples are equivalent)
 
-Lastly, you'll need to pass in the `layout` keyword arg to `render` (or call the controller's class method) with either `modal` or `panel`, depending on which frame is being targeted.
+* `= link_to 'Modal Test', some_modal_layout_action_path, data: { turbo_frame: 'modal' }`
+* `= modal_link_to 'Modal Test', some_modal_layout_action_path`
+
+Panel: (the following 2 examples are equivalent)
+
+* `= link_to 'Panel Test', some_panel_layout_action_path, data: { turbo_frame: 'panel' }`
+* `= panel_link_to 'Panel Test', some_panel_layout_action_path`
+
+You'll then need to pass the `layout` kwarg when calling `render` with either `modal` or `panel`, depending on which frame is being targeted.  (We recommend against the `layout` controller class method, because it causes *layout fallback* issues when the controller is subclassed.)
+
+e.g.
 
 ```ruby
-class SomeController < ApplicationController
+class SomethingsController < ApplicationController
   def new
     render layout: 'modal'
   end
 end
 ```
+
+## TurboFrame modal & panel forms
+
+There are a couple of important rules when it comes to TurboFrames & forms.
+
+1. In the case of errors, your controller action must respond with `status: :unprocessable_entity` *HTTP status code __422__* in order to re-render your form w/ errors.  This is both a Turbo requirement, as well as the mechanism which prevents the modal or panel from re-animating in.
+2. In the case of success, your controller action must redirect.  This is a Turbo requirement in Rails 7+
+
+e.g.
+
+```ruby
+class SomethingsController < ApplicationController
+  def new
+    @something = authorize Something.new
+    render layout: 'modal'
+  end
+
+  def create
+    @something = authorize Something.new(some_params)
+
+    if @something.save
+      redirect_to @something, notice: 'Something created successfully'
+    else
+      render :new, status: :unprocessable_entity, layout: 'modal'
+    end
+  end
+end
+```
+
+## Layout Content Slots
+
+The included modal layout includes *slots* for title content & submit buttons, in addition to the main content `yield`.  If you're use case is simpler, you may remove these sections.  Otherwise, the following is an example edit template meant to be rendered in the modal layout.
+
+```slim
+= content_for :modal_title do
+  h2 Edit the thing
+
+= content_for :modal_actions do
+  = button_tag 'Save', class: 'btn btn--primary', form: dom_id(@thing, :edit)
+
+= simple_form_for @thing do |f|
+  = f.input :name
+  = f.input :description
+```
+
+__note:__ the submit button in the `new.html.slim` version of this template would be `form: dom_id(@thing)` or simply `form: 'new_thing'`.  For further explanation of form Id generation, see the [polymorphic_path docs](https://api.rubyonrails.org/classes/ActionDispatch/Routing/PolymorphicRoutes.html) of simply inspect the form element in your browser.
--- lib/generators/rolemodel/modals/modals_generator.rb
+++ lib/generators/rolemodel/modals/modals_generator.rb
@@ -1,97 +1,68 @@
+
+
 module Rolemodel
   class ModalsGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
-    STYLESHEET_ROOT = 'app/javascript/stylesheets'.freeze
-    STYLESHEET_ENTRYPOINT = 'app/javascript/packs/stylesheets.scss'.freeze
-    OPTICS_MODAL_STYLE_PATH = 'https://raw.githubusercontent.com/RoleModel/optics/main/src/addons/rails-modal-and-panel/modal.scss'.freeze
-    OPTICS_PANEL_STYLE_PATH = 'https://raw.githubusercontent.com/RoleModel/optics/main/src/addons/rails-modal-and-panel/panel.scss'.freeze
+    STYLESHEET_ENTRYPOINT = 'app/assets/stylesheets/application.scss'
+    JAVASCRIPT_ENTRYPOINT = 'app/javascript/application.js'
 
     def install_turbo_confirm
+      say 'Installing Turbo Confirm package', :green
       run 'yarn add @rolemodel/turbo-confirm'
     end
 
-    def install_icons
-      generate 'rolemodel:css:base' unless File.exists?(Rails.root.join('app/javascript/packs/stylesheets.scss'))
-      generate 'rolemodel:css:icons'
-    end
-
-    def add_ui_components
+    def copy_files
+      say 'generating files', :green
       copy_file 'app/helpers/turbo_frame_link_helper.rb'
-      addendum = File.read([source_paths.last, '/application_addendum.js'].join)
-      append_to_file 'app/javascript/packs/application.js', addendum
       copy_file 'app/javascript/controllers/toggle_controller.js'
-      copy_file 'app/views/layouts/application.html.slim'
+      copy_file 'app/javascript/initializers/turbo_confirm.js'
+      copy_file 'app/javascript/initializers/frame_missing_handler.js'
       copy_file 'app/views/layouts/modal.html.slim'
       copy_file 'app/views/layouts/panel.html.slim'
       copy_file 'app/views/shared/_confirm.html.slim'
     end
 
-    def add_css
-      if yes?('Are you using Optics?')
-        import_optics_addons
-      else
-        inject_optics_stylesheets
-      end
+    def amend_javascript_entrypoint
+      say 'importing generated javascript files', :green
+      append_to_file 'app/javascript/application.js', <<~JS
+        import './initializers/turbo_confirm'
+        import './initializers/frame_missing_handler'
+      JS
+    end
 
-      inject_into_file "#{STYLESHEET_ROOT}/variables/tokens.scss", before: "  // Spacing" do
-        optimize_indentation <<~'SCSS', 2
-          // Panel
+    def amend_stylesheet_entrypoint
+      say 'importing stylesheet addons', :green
+      inject_into_file 'app/assets/stylesheets/application.scss', after: "@import '@rolemodel/optics/dist/scss/optics';\n" do
+        optimize_indentation <<~SCSS, 2
+          @import '@rolemodel/optics/dist/scss/addons/rails-modal-and-panel/modal';
+          @import '@rolemodel/optics/dist/scss/addons/rails-modal-and-panel/panel';
+
+          // Panel tokens
           --panel-width: 40%;
           --panel-transition-speed: 400ms;
 
-          // Modals
+          // Modals tokens
           --modal-width: 564px;
           --modal-transition-speed: 300ms;
-
         SCSS
       end
     end
 
-    def add_to_styleguide
-      inject_into_file 'app/controllers/styleguide_controller.rb', after: "def index; end\n" do
-        optimize_indentation <<~'RUBY', 2
-
-          def popup
-            render layout: params[:type]
-          end
-        RUBY
-      end
-
-      route "get '/styleguide/popup/:type', to: 'styleguide#popup', as: :styleguide_popup"
-      copy_file 'app/views/styleguide/popup_content.html.slim'
-      copy_file 'app/views/styleguide/_modals.html.slim'
-      append_to_file 'app/views/styleguide/index.html.slim', "  = render 'modals'"
-    end
-
-    private
-
-    def import_optics_addons
-      inject_into_file STYLESHEET_ENTRYPOINT, after: "@import '@rolemodel/optics/dist/scss/optics';" do
-        optimize_indentation <<~'SCSS', 2
-          @import '@rolemodel/optics/dist/scss/addons/rails-modal-and-panel/modal';
-          @import '@rolemodel/optics/dist/scss/addons/rails-modal-and-panel/panel';
-        SCSS
+    def amend_application_layout
+      say 'altering application layout', :green
+      inject_into_file 'app/views/layouts/application.html.slim', after: /\bbody.*\n/ do
+        optimize_indentation <<~SLIM, 2
+          = turbo_frame_tag 'modal'
+          = turbo_frame_tag 'panel'
+          = render 'shared/confirm'
+        SLIM
       end
     end
 
-    def inject_optics_stylesheets
-      get OPTICS_MODAL_STYLE_PATH do |modal_styles|
-        create_file "#{STYLESHEET_ROOT}/components/modal.scss" do
-          modal_styles
-        end
-      end
-
-      get OPTICS_PANEL_STYLE_PATH do |panel_styles|
-        create_file "#{STYLESHEET_ROOT}/components/panel.scss" do
-          panel_styles
-        end
-      end
-
-      append_to_file STYLESHEET_ENTRYPOINT, <<~SCSS
-        @import 'stylesheets/components/modal.scss';
-        @import 'stylesheets/components/panel.scss';
-      SCSS
+    def register_stimulus_controller
+      say 'updating stimulus manifest', :green
+      run 'rails stimulus:manifest:update'
     end
   end
 end
