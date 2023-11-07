8302f5d58404a1ed99fc970239b9d329ef499160
malachirwin <malachi.irwin@rolemodelsoftware.com>
2023-11-07 16:25:12 -0600

    Cleanup testing setup

--- lib/generators/lightning_cad/install_generator.rb
+++ lib/generators/lightning_cad/install_generator.rb
@@ -31,26 +31,23 @@ module LightningCad
         run "yarn add #{dependencies.join(" ")}"
       end
 
-      def install_yarn_dev_dependencies
-        say 'Adding javascript devDependencies'
-
-        dev_dependencies = %w[
-          jasmine@^4.6.0
-        ]
-        run "yarn add --dev #{dev_dependencies.join(" ")}"
-      end
-
       def install_yarn_optional_dependencies
         say 'Adding optional dependencies'
         say "THREE.js packages are not required if this project does not implement 3D views"
         run "yarn add --optional three@^0.144.0"
       end
 
-      def add_yarn_tasks
-        say 'Adding package.json scripts'
+      def add_jasmine_tests
+        say 'Adding jasmine'
+        run "yarn add --dev jasmine@^5.1.0"
 
-        run 'npm pkg set scripts.build="webpack --config webpack.config.js"'
         run 'npm pkg set scripts.test_shared="NODE_ENV=test NODE_PATH="./node_modules:./app/javascript:$NODE_PATH" jasmine --config=jasmine.json"'
+
+        copy_file 'jasmine.json', 'jasmine.json'
+        copy_file 'spec/javascript/helpers/initializer.js', 'spec/javascript/helpers/initializer.js'
+        copy_file 'spec/javascript/shared/.eslintrc.js', 'spec/javascript/shared/.eslintrc.js'
+        copy_file 'spec/javascript/shared/TestSetup.js', 'spec/javascript/shared/TestSetup.js'
+        copy_file 'spec/javascript/shared/testSpec.js', 'spec/javascript/shared/testSpec.js'
       end
 
       def add_webpack_config
@@ -64,11 +61,10 @@ module LightningCad
         copy_file 'app/javascript/components/MaterialIcon.jsx', 'app/javascript/components/MaterialIcon.jsx'
         copy_file 'app/javascript/components/LocalIconFactory.jsx', 'app/javascript/components/LocalIconFactory.jsx'
         copy_file 'app/javascript/components/App.jsx', 'app/javascript/components/App.jsx'
-        copy_file 'spec/javascript/components/AppSpec.jsx', 'spec/javascript/components/AppSpec.jsx'
       end
 
       def add_stylesheets
-        stylesheets = <<-CSS
+        stylesheets = <<~CSS
           @import '@rolemodel/lightning-cad-ui/scss/lightning-cad.scss';
         CSS
 
