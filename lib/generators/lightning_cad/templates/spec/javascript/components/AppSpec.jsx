0dda6a1283760a0f71ff38c469ad8064b9ab9cf9
malachirwin <malachi.irwin@rolemodelsoftware.com>
2023-11-07 16:25:12 -0600

    Cleanup testing setup

--- lib/generators/lightning_cad/templates/spec/javascript/components/AppSpec.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { render } from "@testing-library/react"
-import App from "components/App.jsx"
-
-describe("App", () => {
-  it("renders without crashing", () => {
-    expect(() => render(<App />)).not.toThrow()
-  })
-})
