cc9debcc811975fe8e8c82c308c9cc359022567a
malachirwin <malachi.irwin@rolemodelsoftware.com>
2023-11-07 16:27:39 -0600

    fix typo

--- lib/generators/lightning_cad/install_generator.rb
+++ lib/generators/lightning_cad/install_generator.rb
@@ -44,7 +44,7 @@ module LightningCad
         run 'npm pkg set scripts.test_shared="NODE_ENV=test NODE_PATH="./node_modules:./app/javascript:$NODE_PATH" jasmine --config=jasmine.json"'
 
         copy_file 'jasmine.json', 'jasmine.json'
-        copy_file 'spec/javascript/helpers/initializer.js', 'spec/javascript/helpers/initializer.js'
+        copy_file 'spec/javascript/helpers/initializers.js', 'spec/javascript/helpers/initializers.js'
         copy_file 'spec/javascript/shared/.eslintrc.js', 'spec/javascript/shared/.eslintrc.js'
         copy_file 'spec/javascript/shared/TestSetup.js', 'spec/javascript/shared/TestSetup.js'
         copy_file 'spec/javascript/shared/testSpec.js', 'spec/javascript/shared/testSpec.js'
