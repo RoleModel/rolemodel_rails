89af37114bdb0735c8cd64ea918ff39c07afca2c
malachirwin <malachi.irwin@rolemodelsoftware.com>
2023-11-29 17:01:15 -0600

    fix all generator

--- lib/generators/lightning_cad/all_generator.rb
+++ lib/generators/lightning_cad/all_generator.rb
@@ -2,7 +2,11 @@ require 'rails'
 
 module LightningCad
   class AllGenerator < Rails::Generators::Base
-    generate 'lightning_cad:install'
-    generate 'lightning_cad:test'
+    source_root File.expand_path('./templates', __dir__)
+
+    def run_all_the_generators
+      generate 'lightning_cad:install'
+      generate 'lightning_cad:test'
+    end
   end
 end
