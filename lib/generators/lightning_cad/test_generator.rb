0bfbc561587b5ddb9418149818689505b20ce565
malachirwin <malachi.irwin@rolemodelsoftware.com>
2023-11-29 16:59:44 -0600

    Remove the extra module around the generators

--- lib/generators/lightning_cad/test_generator.rb
+++ lib/generators/lightning_cad/test_generator.rb
@@ -1,28 +1,26 @@
 require 'rails'
 
 module LightningCad
-  module Generators
-    class TestGenerator < ::Rails::Generators::Base
-      source_root File.expand_path('./templates', __dir__)
+  class TestGenerator < Rails::Generators::Base
+    source_root File.expand_path('./templates', __dir__)
 
-      def add_jasmine_tests
-        say 'Adding jasmine'
-        run "yarn add --dev jasmine@^5.1.0"
+    def add_jasmine_tests
+      say 'Adding jasmine'
+      run "yarn add --dev jasmine@^5.1.0"
 
-        run 'npm pkg set scripts.test_shared="NODE_ENV=test NODE_PATH="./node_modules:./app/javascript:$NODE_PATH" jasmine --config=jasmine.json"'
+      run 'npm pkg set scripts.test_shared="NODE_ENV=test NODE_PATH="./node_modules:./app/javascript:$NODE_PATH" jasmine --config=jasmine.json"'
 
-        copy_file 'jasmine.json', 'jasmine.json'
-        copy_file 'spec/javascript/helpers/initializers.js', 'spec/javascript/helpers/initializers.js'
-        copy_file 'spec/javascript/shared/.eslintrc.js', 'spec/javascript/shared/.eslintrc.js'
-        copy_file 'spec/javascript/shared/TestSetup.js', 'spec/javascript/shared/TestSetup.js'
-        copy_file 'spec/javascript/shared/testSpec.js', 'spec/javascript/shared/testSpec.js'
-      end
+      copy_file 'jasmine.json', 'jasmine.json'
+      copy_file 'spec/javascript/helpers/initializers.js', 'spec/javascript/helpers/initializers.js'
+      copy_file 'spec/javascript/shared/.eslintrc.js', 'spec/javascript/shared/.eslintrc.js'
+      copy_file 'spec/javascript/shared/TestSetup.js', 'spec/javascript/shared/TestSetup.js'
+      copy_file 'spec/javascript/shared/testSpec.js', 'spec/javascript/shared/testSpec.js'
+    end
 
-      def add_test_initializers
-        say 'Adding test initializers'
+    def add_test_initializers
+      say 'Adding test initializers'
 
-        copy_file 'spec/javascript/helpers/initializers.js', 'spec/javascript/helpers/initializers.js'
-      end
+      copy_file 'spec/javascript/helpers/initializers.js', 'spec/javascript/helpers/initializers.js'
     end
   end
 end
