82530e7e20cf77191d3fdc0c69201f8ba00379fa
malachirwin <malachi.irwin@rolemodelsoftware.com>
2023-11-29 16:46:08 -0600

    rename the jasmine generator to test

--- lib/generators/lightning_cad/all_generator.rb
+++ lib/generators/lightning_cad/all_generator.rb
@@ -4,7 +4,7 @@ module LightningCad
   module Generators
     class AllGenerator < ::Rails::Generators::Base
       generate 'lightning_cad:install'
-      generate 'lightning_cad:jasmine'
+      generate 'lightning_cad:test'
     end
   end
 end
