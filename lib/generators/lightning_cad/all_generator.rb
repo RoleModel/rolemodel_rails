b0263cf1bf36bd1819395371feae9369e4b061a8
malachirwin <malachi.irwin@rolemodelsoftware.com>
2023-11-29 16:59:44 -0600

    Remove the extra module around the generators

--- lib/generators/lightning_cad/all_generator.rb
+++ lib/generators/lightning_cad/all_generator.rb
@@ -1,10 +1,8 @@
 require 'rails'
 
 module LightningCad
-  module Generators
-    class AllGenerator < ::Rails::Generators::Base
-      generate 'lightning_cad:install'
-      generate 'lightning_cad:test'
-    end
+  class AllGenerator < Rails::Generators::Base
+    generate 'lightning_cad:install'
+    generate 'lightning_cad:test'
   end
 end
