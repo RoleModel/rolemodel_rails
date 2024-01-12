91b02d22119dd566abbf25c9be7665bcecf13e9b
Andy Cohen <outlawandy@gmail.com>
2024-01-12 13:41:47 -0600

    --wip-- [skip ci]

--- lib/generators/rolemodel/modals/templates/app/helpers/turbo_frame_link_helper.rb
+++ lib/generators/rolemodel/modals/templates/app/helpers/turbo_frame_link_helper.rb
@@ -1,9 +1,9 @@
 module TurboFrameLinkHelper
-  def panel_link_to(name, url=nil, options={}, &block)
+  def panel_link_to(name, url={}, options=url, &block)
     frame_link_helper('panel', name, url, options, &block)
   end
 
-  def modal_link_to(name, url=nil, options={}, &block)
+  def modal_link_to(name, url={}, options=url, &block)
     frame_link_helper('modal', name, url, options, &block)
   end
 
@@ -13,7 +13,7 @@ module TurboFrameLinkHelper
     data_option = { data: options.fetch(:data, {}).reverse_merge({ turbo_frame: frame }) }
 
     if block_given?
-      link_to(name, (url || {}).merge(data_option), &block)
+      link_to(name, url.merge(data_option), &block)
     else
       link_to(name, url, options.merge(data_option))
     end
