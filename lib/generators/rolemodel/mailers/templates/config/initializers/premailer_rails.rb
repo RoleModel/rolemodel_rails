945fc5f79db8ea2419ac30858c15d66bfcab7410
John Calvin Young <john.young@rolemodelsoftware.com>
2024-07-30 20:18:07 -0400

    Improve the regex to remove custom properties cleanly, without leaving broken syntax when parsing Optics

--- lib/generators/rolemodel/mailers/templates/config/initializers/premailer_rails.rb
+++ lib/generators/rolemodel/mailers/templates/config/initializers/premailer_rails.rb
@@ -2,7 +2,7 @@ module CustomPropertyCSSHelper
   def load_css(url)
     # strip out any CSS Custom Properties, PostCSS includes the fallbacks,
     # but Premailer can't ignore them so we remove them
-    super.gsub(/(\b[a-z_-]*?:[^\n;{}]*var\(.*?)?\B--.*?(;|(?=\}))/, '')
+    super.gsub(/((\b[a-z_-]*?:)?[^\n;{}]*var\(.*?)?\B--.*?(,(?=\n)|;(?!\S)|(?=\}))/, '')
   end
 end
 
