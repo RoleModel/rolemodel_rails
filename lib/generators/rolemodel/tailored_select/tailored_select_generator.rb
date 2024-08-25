c6d7a7bb3ed2ea1061d157df0b9a790bae987348
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2024-08-25 17:58:36 -0400

    Add test for tailored select generator

--- lib/generators/rolemodel/tailored_select/tailored_select_generator.rb
+++ lib/generators/rolemodel/tailored_select/tailored_select_generator.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 module Rolemodel
-  module Optics
-    class TailoredSelectGenerator < Rails::Generators::Base
-      source_root File.expand_path('templates', __dir__)
+  class TailoredSelectGenerator < Rails::Generators::Base
+    source_root File.expand_path('templates', __dir__)
 
-      def add_tailored_select_package
-        say 'Installing Tailored Select package', :green
+    def add_tailored_select_package
+      say 'Installing Tailored Select package', :green
 
-        run 'yarn add @rolemodel/tailored-select'
-      end
+      run 'yarn add @rolemodel/tailored-select'
     end
   end
 end
---
+++ spec/generators/rolemodel/tailored_select_generator_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'generators/rolemodel/tailored_select/tailored_select_generator'
+
+RSpec.describe Rolemodel::TailoredSelectGenerator, type: :generator do
+  destination File.expand_path('tmp/', File.dirname(__FILE__))
+
+  before(:all) do
+    prepare_test_app
+    run_generator
+  end
+
+  after(:all) do
+    cleanup_test_app
+  end
+
+  it 'adds tailored select to package.json' do
+    assert_file 'package.json' do |content|
+      expect(content).to include('"@rolemodel/tailored-select":')
+    end
+  end
+end
