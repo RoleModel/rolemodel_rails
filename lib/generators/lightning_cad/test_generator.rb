b44083909c21edc5670810dca709f54d0ab8550d
Malachi Irwin <malachi.irwin@rolemodelsoftware.com>
2024-10-25 16:44:34 -0500

    Add browser testing

--- lib/generators/lightning_cad/test_generator.rb
+++ lib/generators/lightning_cad/test_generator.rb
@@ -6,21 +6,19 @@ module LightningCad
 
     def add_jasmine_tests
       say 'Adding jasmine'
-      run "yarn add --dev jasmine@^5.1.0"
+      run "yarn add --dev jasmine@^5.1.0 @rolemodel/jasmine-playwright-runner @testing-library/react"
 
-      run 'npm pkg set scripts.test_shared="NODE_OPTIONS=\'--import=./app/javascript/helpers/register_hooks.js\' jasmine --config=jasmine.json"'
+      run 'npm pkg set scripts.test:server="NODE_OPTIONS=\'--import=./app/javascript/helpers/register_hooks.js\' jasmine --config=jasmine.json"'
+      run 'npm pkg set scripts.test:browser="NODE_ENV=test jp-runner"'
+      run 'npm pkg set scripts.test="yarn test:server && yarn test:browser"'
 
-      copy_file 'jasmine.json', 'jasmine.json'
-      copy_file 'spec/javascript/helpers/initializers.js', 'spec/javascript/helpers/initializers.js'
-      copy_file 'spec/javascript/shared/.eslintrc.js', 'spec/javascript/shared/.eslintrc.js'
-      copy_file 'spec/javascript/shared/TestSetup.js', 'spec/javascript/shared/TestSetup.js'
-      copy_file 'spec/javascript/shared/testSpec.js', 'spec/javascript/shared/testSpec.js'
-    end
-
-    def add_test_initializers
-      say 'Adding test initializers'
-
-      copy_file 'spec/javascript/helpers/initializers.js', 'spec/javascript/helpers/initializers.js'
+      copy_file 'jasmine.json'
+      copy_file 'jp-runner.config.json'
+      copy_file 'spec/javascript/.eslintrc.js'
+      copy_file 'spec/javascript/shared/TestSetup.js'
+      copy_file 'spec/javascript/browser/TestSetup.js'
+      copy_file 'spec/javascript/server/TestSetup.js'
+      copy_file 'spec/javascript/shared/testSpec.js'
     end
   end
 end
