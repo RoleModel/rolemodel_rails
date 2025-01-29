a960a7e1f2f86cc914fb65d5efbd024e930d5805
Andy Cohen <outlawandy@gmail.com>
2025-01-29 17:35:06 -0500

    Update Modals Generator (#136)

---
+++ spec/generators/rolemodel/modal_generator_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'generators/rolemodel/modals/modals_generator'
+
+RSpec.describe Rolemodel::ModalsGenerator, type: :generator do
+  destination File.expand_path('tmp/', File.dirname(__FILE__))
+
+  before { run_generator_against_test_app }
+
+  it 'adds the correct javascript files' do
+    assert_file 'app/javascript/controllers/toggle_controller.js'
+    assert_file 'app/javascript/initializers/turbo_confirm.js'
+    assert_file 'app/javascript/initializers/frame_missing_handler.js'
+    assert_file 'app/javascript/initializers/before_morph_handler.js'
+  end
+
+  it 'correctly appends to the application.js file' do
+    assert_file 'app/javascript/application.js' do |content|
+      expect(content).to include("import './initializers/turbo_confirm.js'")
+      expect(content).to include("import './initializers/frame_missing_handler.js'")
+      expect(content).to include("import './initializers/before_morph_handler.js'")
+    end
+  end
+
+  it 'correctly adds view templates' do
+    assert_file 'app/views/layouts/modal.html.slim'
+    assert_file 'app/views/layouts/panel.html.slim'
+    assert_file 'app/views/application/_confirm.html.slim'
+  end
+end
