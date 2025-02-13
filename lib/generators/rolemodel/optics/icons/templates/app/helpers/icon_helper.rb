0e660009de030410e3c0fe02f1656e1b987955cd
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-02-13 13:20:01 -0500

    Address comments

--- lib/generators/rolemodel/optics/icons/templates/app/helpers/icon_helper.rb
+++ lib/generators/rolemodel/optics/icons/templates/app/helpers/icon_helper.rb
@@ -3,16 +3,16 @@
 # Helper methods to render icons from different icon libraries.
 # Usage:
 # You can use the `icon` helper method to render icons from the default library.
-# = icon('home', size: 'x-large, color: 'primary')
+# = icon('home', size: 'x-large', color: 'primary')
 # The default library is Material Icons, but can be changed here by changing the alias.
 #
 # You can also use the specific icon helper methods to render icons from a specific library.
-# = material_icon('home', size: 'x-large, color: 'primary')
-# = phosphor_icon('home', size: 'x-large, color: 'primary')
-# = tabler_icon('home', size: 'x-large, color: 'primary')
-# = feather_icon('home', size: 'x-large, color: 'primary')
-# = lucide_icon('home', size: 'x-large, color: 'primary')
-# = custom_icon('home', size: 'x-large, color: 'primary')
+# = material_icon('home', size: 'x-large', color: 'primary')
+# = phosphor_icon('home', size: 'x-large', color: 'primary')
+# = tabler_icon('home', size: 'x-large', color: 'primary')
+# = feather_icon('home', size: 'x-large', color: 'primary')
+# = lucide_icon('home', size: 'x-large', color: 'primary')
+# = custom_icon('home', size: 'x-large', color: 'primary')
 module IconHelper
   def icon_name_for_flash(type)
     # NOTE: This assumes material icon names
@@ -58,4 +58,7 @@ module IconHelper
 
   # Set the default to Material Icons
   alias icon material_icon
+  # Might be more interesting to look into https://www.rubydoc.info/gems/thor/Thor/Shell/Basic#ask-instance_method
+  # We could prompt which icon library they'd like the generator to "install" and inject this line dynamically based on
+  # what's selected. Maybe also comment out or not bring in the icon packs that are not being used.
 end
