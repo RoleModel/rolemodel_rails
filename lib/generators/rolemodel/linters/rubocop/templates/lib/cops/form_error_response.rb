ad84dc99baec93da9697b2aef978e1c5fe0d8d2f
Michael Hale <mike@hales.ws>
2025-07-07 10:24:12 -0400

    Move custom cops to Cops namespace

--- lib/generators/rolemodel/linters/rubocop/templates/lib/cops/form_error_response.rb
+++ lib/generators/rolemodel/linters/rubocop/templates/lib/cops/form_error_response.rb
@@ -13,25 +13,27 @@
 # This checks the specific matching line to ensure it meets our expectations
 
 # Custom Cop to Ensure Form Error Response Statuses
-class FormErrorResponse < RuboCop::Cop::Base
-  # Match code in the else branch that either: is the only line and looks like `render :something, ...`
-  # or is not the first line and looks like the same.
-  def_node_matcher :form_error, <<~PATTERN
-    {(... $(send nil? :render (:sym _) ...)) | $(send nil? :render (:sym _) ...)}
-  PATTERN
+module Cops
+  class FormErrorResponse < RuboCop::Cop::Base
+    # Match code in the else branch that either: is the only line and looks like `render :something, ...`
+    # or is not the first line and looks like the same.
+    def_node_matcher :form_error, <<~PATTERN
+      {(... $(send nil? :render (:sym _) ...)) | $(send nil? :render (:sym _) ...)}
+    PATTERN
 
-  MSG = 'Use status: :unprocessable_entity for invalid form requests.'
+    MSG = 'Use status: :unprocessable_entity for invalid form requests.'
 
-  # Ensure the render is returning a status of unprocessable entity,
-  # otherwise add the offense
-  STATUS_PAIR = /\(pair\s+\(sym :status\)\s+\(sym :unprocessable_entity\)\)/m
+    # Ensure the render is returning a status of unprocessable entity,
+    # otherwise add the offense
+    STATUS_PAIR = /\(pair\s+\(sym :status\)\s+\(sym :unprocessable_entity\)\)/m
 
-  # Only check within if blocks
-  def on_if(node)
-    form_error(node.else_branch) do |name|
-      next if name.to_s.match?(STATUS_PAIR)
+    # Only check within if blocks
+    def on_if(node)
+      form_error(node.else_branch) do |name|
+        next if name.to_s.match?(STATUS_PAIR)
 
-      add_offense(name)
+        add_offense(name)
+      end
     end
   end
 end
--- lib/generators/rolemodel/linters/rubocop/templates/lib/cops/no_chrome_tag.rb
+++ lib/generators/rolemodel/linters/rubocop/templates/lib/cops/no_chrome_tag.rb
@@ -2,41 +2,43 @@
 
 require 'rubocop'
 
-class NoChromeTag < RuboCop::Cop::Base
-  include RangeHelp
-  extend AutoCorrector
+module Cops
+  class NoChromeTag < RuboCop::Cop::Base
+    include RuboCop::Cop::RangeHelp
+    extend RuboCop::Cop::AutoCorrector
 
-  MSG = 'The :chrome tag is only for testing, and should not be checked into the repository.'
-  RESTRICT_ON_SEND = [:describe, :context, :it, :feature, :scenario].freeze
+    MSG = 'The :chrome tag is only for testing, and should not be checked into the repository.'
+    RESTRICT_ON_SEND = %i[describe context it feature scenario].freeze
 
-  def on_send(node)
-    node.arguments.each do |a|
-      if a.sym_type? && a.value == :chrome
-        range = range_with_surrounding_comma(a.source_range)
-        add_offense(range) do |corrector|
-          corrector.replace(range, '')
+    def on_send(node)
+      node.arguments.each do |a|
+        if a.sym_type? && a.value == :chrome
+          range = range_with_surrounding_comma(a.source_range)
+          add_offense(range) do |corrector|
+            corrector.replace(range, '')
+          end
         end
-      end
 
-      next unless a.hash_type?
+        next unless a.hash_type?
 
-      a.pairs.each do |pair|
-        next unless pair.key.value == :chrome
+        a.pairs.each do |pair|
+          next unless pair.key.value == :chrome
 
-        range = range_with_surrounding_comma(pair.source_range)
-        add_offense(range) do |corrector|
-          corrector.replace(range, '')
+          range = range_with_surrounding_comma(pair.source_range)
+          add_offense(range) do |corrector|
+            corrector.replace(range, '')
+          end
         end
       end
     end
-  end
 
-  def range_with_surrounding_comma(source_range)
-    buffer = source_range.source_buffer
-    src = buffer.source
-    end_pos = source_range.end_pos
-    comma_index = src[...end_pos].rindex(/\s*,\s*/)
+    def range_with_surrounding_comma(source_range)
+      buffer = source_range.source_buffer
+      src = buffer.source
+      end_pos = source_range.end_pos
+      comma_index = src[...end_pos].rindex(/\s*,\s*/)
 
-    Parser::Source::Range.new(buffer, comma_index, end_pos)
+      Parser::Source::Range.new(buffer, comma_index, end_pos)
+    end
   end
 end
