ab9a5160c0ed1fa95405747a4b2dbbdda8e4e6dd
Nathan Sadler <nathan@sadlerville.com>
2025-08-06 13:33:59 -0400

    Add alias for running tests

--- lib/generators/rolemodel/jasmine_playwright/jasmine_playwright_generator.rb
+++ lib/generators/rolemodel/jasmine_playwright/jasmine_playwright_generator.rb
@@ -10,6 +10,21 @@ module Rolemodel
       lit-html
     ]
 
+    def add_jasmine_playwright_script
+      package_json_path = Rails.root.join('package.json')
+      command = 'NODE_ENV=test jp-runner --config jp-runner.config.mjs --webpack-config webpack.config.cjs'
+
+      if File.exist?(package_json_path)
+        package_json = JSON.parse(File.read(package_json_path))
+        package_json['scripts'] ||= {}
+        package_json['scripts']['test:browser'] = command
+        File.write(package_json_path, JSON.generate(package_json))
+        say 'Added jasmine:playwright script to package.json', :green
+      else
+        say 'package.json not found. Please run yarn init first.', :red
+      end
+    end
+
     def ensure_node_version
       say "Establish development environment Node version of #{set_color(NODE_VERSION, :yellow)}", :green
 
