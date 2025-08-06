15cfa5e34be7a8f10353eecfde18f7a63d21799e
Nathan Sadler <nathan@sadlerville.com>
2025-08-06 13:37:38 -0400

    Appease linter and get to live another day

--- lib/generators/rolemodel/jasmine_playwright/jasmine_playwright_generator.rb
+++ lib/generators/rolemodel/jasmine_playwright/jasmine_playwright_generator.rb
@@ -1,28 +1,28 @@
+# frozen_string_literal: true
+
 module Rolemodel
   class JasminePlaywrightGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
-    NODE_VERSION = '22.15.0'.freeze
+    NODE_VERSION = '22.15.0'
 
     DEV_DEPENDENCIES = %w[
       @rolemodel/jasmine-playwright-runner
       playwright
       lit-html
-    ]
+    ].freeze
 
     def add_jasmine_playwright_script
       package_json_path = Rails.root.join('package.json')
       command = 'NODE_ENV=test jp-runner --config jp-runner.config.mjs --webpack-config webpack.config.cjs'
 
-      if File.exist?(package_json_path)
-        package_json = JSON.parse(File.read(package_json_path))
-        package_json['scripts'] ||= {}
-        package_json['scripts']['test:browser'] = command
-        File.write(package_json_path, JSON.generate(package_json))
-        say 'Added jasmine:playwright script to package.json', :green
-      else
-        say 'package.json not found. Please run yarn init first.', :red
-      end
+      return say 'package.json not found. Please run yarn init first.', :red unless File.exist?(package_json_path)
+
+      package_json = JSON.parse(File.read(package_json_path))
+      package_json['scripts'] ||= {}
+      package_json['scripts']['test:browser'] = command
+      File.write(package_json_path, JSON.generate(package_json))
+      say 'Added jasmine:playwright script to package.json', :green
     end
 
     def ensure_node_version
