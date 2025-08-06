fd85f9ac3ad6f0f24ee29ef9e653dc6c7ca2c3c9
Nathan Sadler <nathan@sadlerville.com>
2025-08-06 09:40:09 -0400

    Add some packages via the generator

--- lib/generators/rolemodel/jasmine_playwright/jasmine_playwright_generator.rb
+++ lib/generators/rolemodel/jasmine_playwright/jasmine_playwright_generator.rb
@@ -1,5 +1,15 @@
 module Rolemodel
   class JasminePlaywrightGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
+
+    DEV_DEPENDENCIES = %w[
+      @rolemodel/jasmine-playwright-runner
+      playwright
+    ]
+
+    def add_npm_packages
+      say 'Adding new dev dependency to package.json', :green
+      run "yarn add --dev #{DEV_DEPENDENCIES.join(' ')}"
+    end
   end
 end
