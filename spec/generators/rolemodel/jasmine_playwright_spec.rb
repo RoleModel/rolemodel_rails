bf4934b91d93a399afc5cb815743830e503d1db2
Nathan Sadler <nathan@sadlerville.com>
2025-08-06 09:20:23 -0400

    Start adding jasmine playwright generator

---
+++ spec/generators/rolemodel/jasmine_playwright_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'generators/rolemodel/jasmine_playwright/jasmine_playwright_generator'
+
+RSpec.describe Rolemodel::JasminePlaywrightGenerator, type: :generator do
+  destination File.expand_path('tmp/', File.dirname(__FILE__))
+
+  before do
+    run_generator_against_test_app
+  end
+
+  it 'adds the correct files' do
+    assert_file '.node-version'
+  end
+end
