e83637bd54b3523ece40daa7278a9d64af1a4a09
Nathan Sadler <nathan@sadlerville.com>
2025-08-06 09:58:04 -0400

    Ensure node version

--- lib/generators/rolemodel/jasmine_playwright/jasmine_playwright_generator.rb
+++ lib/generators/rolemodel/jasmine_playwright/jasmine_playwright_generator.rb
@@ -2,10 +2,18 @@ module Rolemodel
   class JasminePlaywrightGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
+    NODE_VERSION = '22.15.0'.freeze
+
     DEV_DEPENDENCIES = %w[
       @rolemodel/jasmine-playwright-runner
       playwright
     ]
+
+    def ensure_node_version
+      say "Establish development environment Node version of #{set_color(NODE_VERSION, :yellow)}", :green
+
+      create_file '.node-version', NODE_VERSION
+    end
 
     def add_npm_packages
       say 'Adding new dev dependency to package.json', :green
