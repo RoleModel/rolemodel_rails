003ac98fca9c1a45c954d47c97075d636011892b
Jullian Calkins <jullian0426@yahoo.com>
2025-08-06 14:00:10 -0400

    Increment version number

--- lib/generators/rolemodel/testing/rspec/rspec_generator.rb
+++ lib/generators/rolemodel/testing/rspec/rspec_generator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../../../bundler_helpers'
 
 module Rolemodel
--- spec/generators/rolemodel/rspec_generator_spec.rb
+++ spec/generators/rolemodel/rspec_generator_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'generators/rolemodel/testing/rspec/rspec_generator'
 
