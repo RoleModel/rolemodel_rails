05b9fdc0caef5498344437939d21575a40586a7b
Andy Cohen <outlawandy@gmail.com>
2025-10-28 19:59:03 -0500

    additional helper specs

--- spec/helpers/frame_link_helper_spec.rb
+++ spec/helpers/frame_link_helper_spec.rb
@@ -6,15 +6,129 @@ RSpec.describe TurboFrameLinkHelper, type: :helper do
   include ActionView::Helpers
   include TurboFrameLinkHelper
 
-  it 'generates the correct link HTML' do
-    no_block_modal_link_with_options = modal_link_to('My Link', '/path', class: ['true--class' => true, 'false--class' => false], data: { foo: 'bar' }, disabled: false)
-    no_block_panel_link_no_options = panel_link_to('My Link', '/path')
-    block_top_link_with_options = link_to_top('/path', class: ['always--class', 'true--class' => true, 'false--class' => false], disabled: true) { 'My Link' }
-    block_modal_link_no_options = modal_link_to('/path') { 'My Link' }
+  let(:text) { { input: 'My Link', output: 'My Link' } }
+  let(:path) { { input: '/path', output: 'href="/path"' } }
+  let(:classes) { { input: { class: ['always--class', 'true--class' => true, 'false--class' => false] }, output: 'class="always--class true--class"' } }
+  let(:data) { { input: { data: { foo: 'bar' } }, output: 'data-foo="bar"' } }
+  let(:disabled) { { input: { disabled: true }, output: 'disabled="disabled"' } }
 
-    expect(no_block_modal_link_with_options).to eq('<a class="true--class" data-turbo-frame="modal" data-foo="bar" href="/path">My Link</a>')
-    expect(no_block_panel_link_no_options).to eq('<a data-turbo-frame="panel" href="/path">My Link</a>')
-    expect(block_top_link_with_options).to eq('<a class="always--class true--class" disabled="disabled" data-turbo-frame="_top" href="/path">My Link</a>')
-    expect(block_modal_link_no_options).to eq('<a data-turbo-frame="modal" href="/path">My Link</a>')
+  let(:turbo_frame) { { modal: 'data-turbo-frame="modal"', panel: 'data-turbo-frame="panel"', top: 'data-turbo-frame="_top"' } }
+
+  context 'with a block' do
+    context 'with various options' do
+      it 'generates the correct modal link HTML' do
+        options = classes[:input].merge(data[:input])
+
+        expect(
+          modal_link_to(path[:input], options) { tag.i(text[:input]) }
+        ).to eq(
+          "<a #{classes[:output]} #{turbo_frame[:modal]} #{data[:output]} #{path[:output]}><i>#{text[:output]}</i></a>"
+        )
+      end
+
+      it 'generates the correct panel and top link HTML' do
+        options = data[:input].merge(disabled[:input])
+
+        expect(
+          panel_link_to(path[:input], options) { text[:input] }
+        ).to eq(
+          "<a #{disabled[:output]} #{turbo_frame[:panel]} #{data[:output]} #{path[:output]}>#{text[:output]}</a>"
+        )
+      end
+
+      it 'generates the correct top link HTML' do
+        options = classes[:input].merge(disabled[:input]).merge(data[:input])
+
+        expect(
+          link_to_top(path[:input], options) { tag.div(text[:input], class: 'text--class') }
+        ).to eq(
+          "<a #{classes[:output]} #{disabled[:output]} #{turbo_frame[:top]} #{data[:output]} #{path[:output]}><div class=\"text--class\">#{text[:output]}</div></a>"
+        )
+      end
+    end
+
+    context 'with no options' do
+      it 'generates the correct modal link HTML' do
+        expect(
+          modal_link_to(path[:input]) { text[:input] }
+        ).to eq(
+          "<a #{turbo_frame[:modal]} #{path[:output]}>#{text[:output]}</a>"
+        )
+      end
+
+      it 'generates the correct panel link HTML' do
+        expect(
+          panel_link_to(path[:input]) { tag.div(text[:input], **classes[:input]) }
+        ).to eq(
+          "<a #{turbo_frame[:panel]} #{path[:output]}><div #{classes[:output]}>#{text[:output]}</div></a>"
+        )
+      end
+
+      it 'generates the correct top link HTML' do
+        expect(
+          link_to_top(path[:input]) { text[:input] }
+        ).to eq(
+          "<a #{turbo_frame[:top]} #{path[:output]}>#{text[:output]}</a>"
+        )
+      end
+    end
+  end
+
+  context 'without a block' do
+    context 'with various options' do
+      it 'generates the correct modal link HTML' do
+        expect(
+          modal_link_to(text[:input], path[:input], classes[:input])
+        ).to eq(
+          "<a #{classes[:output]} #{turbo_frame[:modal]} #{path[:output]}>#{text[:output]}</a>"
+        )
+      end
+
+      it 'generates the correct panel link HTML' do
+        options = data[:input].merge(disabled[:input])
+
+        expect(
+          panel_link_to(text[:input], path[:input], options)
+        ).to eq(
+          "<a #{disabled[:output]} #{turbo_frame[:panel]} #{data[:output]} #{path[:output]}>#{text[:output]}</a>"
+        )
+      end
+
+      it 'generates the correct top link HTML' do
+        options = classes[:input].merge(disabled[:input]).merge(data[:input])
+
+        expect(
+          link_to_top(text[:input], path[:input], options)
+        ).to eq(
+          "<a #{classes[:output]} #{disabled[:output]} #{turbo_frame[:top]} #{data[:output]} #{path[:output]}>#{text[:output]}</a>"
+        )
+      end
+    end
+
+    context 'with no options' do
+      it 'generates the correct modal link HTML' do
+        expect(
+          modal_link_to(text[:input], path[:input])
+        ).to eq(
+          "<a #{turbo_frame[:modal]} #{path[:output]}>#{text[:output]}</a>"
+        )
+      end
+
+      it 'generates the correct panel link HTML' do
+        expect(
+          panel_link_to(text[:input], path[:input])
+        ).to eq(
+          "<a #{turbo_frame[:panel]} #{path[:output]}>#{text[:output]}</a>"
+        )
+      end
+
+      it 'generates the correct top link HTML' do
+        expect(
+          link_to_top(tag.div(text[:input], **classes[:input]), path[:input])
+        ).to eq(
+          "<a #{turbo_frame[:top]} #{path[:output]}><div #{classes[:output]}>#{text[:output]}</div></a>"
+        )
+      end
+    end
   end
 end
