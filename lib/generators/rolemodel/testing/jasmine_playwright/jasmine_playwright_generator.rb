5eefeca7a4eef18c00c66fd0dca0da58ae48b6e0
Justin Wiebe <justin.wiebe@rolemodelsoftware.com>
2026-01-30 13:21:36 -0600

    Add prompt around jasmine playwright generator to allow skipping it when installing all generators

--- lib/generators/rolemodel/testing/jasmine_playwright/jasmine_playwright_generator.rb
+++ lib/generators/rolemodel/testing/jasmine_playwright/jasmine_playwright_generator.rb
@@ -5,7 +5,8 @@ module Rolemodel
     class JasminePlaywrightGenerator < Rails::Generators::Base
       include ReplaceContentHelper
       source_root File.expand_path('templates', __dir__)
-      class_option :github_package_token, type: :string, default: ENV['GITHUB_PACKAGES_TOKEN'], desc: 'GitHub Packages token with access to @rolemodel packages'
+      class_option :github_package_token, type: :string, default: ENV['GITHUB_PACKAGES_TOKEN'],
+                                          desc: 'GitHub Packages token with access to @rolemodel packages'
 
       TEST_COMMAND = 'NODE_ENV=test jp-runner --config jp-runner.config.mjs --webpack-config webpack.config.cjs'
       DEV_DEPENDENCIES = %w[
@@ -15,14 +16,23 @@ module Rolemodel
       ].freeze
 
       def fail_without_github_token
-        raise 'a --github_package_token option or GITHUB_PACKAGES_TOKEN environment variable is required' if options[:github_package_token].blank?
+        @add_jasmine = yes?('Would you like to add jasmine-playwright-runner for browser testing?')
+        return unless @add_jasmine
+
+        return unless options[:github_package_token].blank?
+
+        raise 'a --github_package_token option or GITHUB_PACKAGES_TOKEN environment variable is required'
       end
 
       def yarn_init_unless_package_json_exists
+        return unless @add_jasmine
+
         run 'yarn init' unless File.exist?(File.expand_path('package.json', destination_root))
       end
 
       def add_browser_test_script
+        return unless @add_jasmine
+
         say 'Adding yarn test:browser command', :green
 
         replace_content('package.json') do |json|
@@ -34,18 +44,24 @@ module Rolemodel
       end
 
       def set_node_version
+        return unless @add_jasmine
+
         say "Establish development environment Node version of #{set_color(NODE_VERSION, :yellow)}", :green
 
         create_file '.node-version', NODE_VERSION, force: true
       end
 
       def add_dev_dependencies
+        return unless @add_jasmine
+
         say 'Adding new dev dependency to package.json', :green
 
         run "yarn add --dev #{DEV_DEPENDENCIES.join(' ')}"
       end
 
       def add_spec_config_files
+        return unless @add_jasmine
+
         say 'Adding Jasmine Playwright configuration files', :green
 
         template 'example_spec.js', 'spec/javascript/browser/example_spec.js'
