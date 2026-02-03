680a68cf8526129b7e9eb1dd6dbc83751d716f9d
Andy Cohen <outlawandy@gmail.com>
2026-02-02 20:16:36 -0600

    Upgrade Dependencies, Example Apps, Add bin/bump_version script (#170)

--- lib/generators/rolemodel.rb
+++ lib/generators/rolemodel.rb
@@ -1,5 +1,8 @@
+require 'pathname'
+
 module Rolemodel
-  NODE_VERSION = '22.15.0'
+  RUBY_VERSION = Pathname.pwd.join('.ruby-version').read.strip
+  NODE_VERSION = Pathname.pwd.join('.node-version').read.strip
 
   autoload :BundlerHelpers, File.expand_path('./bundler_helpers', __dir__)
   autoload :ReplaceContentHelper, File.expand_path('./replace_content_helper', __dir__)
