392c6de365916194379d4de3c3f26d42a9e671f6
Andy Cohen <outlawandy@gmail.com>
2026-04-03 20:11:12 -0500

    fix spec issue

--- lib/rolemodel/generator_base.rb
+++ lib/rolemodel/generator_base.rb
@@ -4,12 +4,14 @@ require 'rails/generators/base'
 require 'rails/generators/bundle_helper'
 require_relative 'replace_content_helper'
 
-class Rolemodel::GeneratorBase < Rails::Generators::Base
-  include Rails::Generators::BundleHelper, ReplaceContentHelper
+module Rolemodel
+  class GeneratorBase < ::Rails::Generators::Base
+    include ::Rails::Generators::BundleHelper, ReplaceContentHelper
 
-  private
-  # based on https://github.com/rails/rails/blob/main/railties/lib/rails/generators/app_base.rb#L713
-  def run_bundle
-    bundle_command("install --quiet", "BUNDLE_IGNORE_MESSAGES" => "1")
+    private
+    # based on https://github.com/rails/rails/blob/main/railties/lib/rails/generators/app_base.rb#L713
+    def run_bundle
+      bundle_command("install --quiet", "BUNDLE_IGNORE_MESSAGES" => "1")
+    end
   end
 end
