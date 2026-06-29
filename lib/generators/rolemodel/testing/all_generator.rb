754b0567baf5b8ae0a970cc1aa8d36ce9dfeea4a
Andy Cohen <outlawandy@gmail.com>
2026-06-29 17:12:09 -0500

    Update RSpec Generator

--- lib/generators/rolemodel/testing/all_generator.rb
+++ lib/generators/rolemodel/testing/all_generator.rb
@@ -6,11 +6,11 @@ module Rolemodel
       class_option :js_runner, type: :boolean, default: false, desc: 'Include jasmine-playwright-runner for browser testing'
 
       def run_all_the_generators
-        generate 'rolemodel:testing:factory_bot', [inline: false]
-        generate('rolemodel:testing:jasmine_playwright', [inline: false]) if options.js_runner?
-        generate 'rolemodel:testing:parallel_tests', [inline: false]
-        generate 'rolemodel:testing:rspec', [inline: false]
-        generate 'rolemodel:testing:test_prof', [inline: false]
+        generate 'rolemodel:testing:factory_bot'
+        generate 'rolemodel:testing:jasmine_playwright' if options.js_runner?
+        generate 'rolemodel:testing:parallel_tests'
+        generate 'rolemodel:testing:rspec'
+        generate 'rolemodel:testing:test_prof'
       end
     end
   end
--- spec/generators/rolemodel/testing/rspec_generator_spec.rb
+++ spec/generators/rolemodel/testing/rspec_generator_spec.rb
@@ -1,11 +1,33 @@
 RSpec.describe Rolemodel::Testing::RspecGenerator, type: :generator do
-  before { run_generator_against_test_app }
+  describe 'by default' do
+    before { run_generator_against_test_app }
 
-  it 'adds the correct helpers' do
-    assert_file 'spec/support/helpers/action_cable_helper.rb'
-    assert_file 'spec/support/helpers/capybara_helper.rb'
-    assert_file 'spec/support/helpers/playwright_helper.rb'
-    assert_file 'spec/support/helpers/select_helper.rb'
-    assert_file 'spec/support/helpers/test_element_helper.rb'
+    it 'adds the correct helpers' do
+      assert_file 'spec/support/helpers/action_cable_helper.rb'
+      assert_file 'spec/support/helpers/capybara_helper.rb'
+      assert_file 'spec/support/helpers/playwright_helper.rb'
+      assert_file 'spec/support/helpers/select_helper.rb'
+      assert_file 'spec/support/helpers/test_element_helper.rb'
+    end
+
+    it 'adds correct dependencies' do
+      assert_file 'Gemfile' do |content|
+        expect(content).to match(/rspec-rails/)
+        expect(content).not_to match(/marsh_grass/)
+        expect(content).not_to match(/pry/)
+      end
+    end
+  end
+
+  describe 'with marsh_grass option' do
+    before { run_generator_against_test_app %w[--marsh_grass] }
+
+    it 'adds the additional gems' do
+      assert_file 'Gemfile' do |content|
+        expect(content).to match(/rspec-rails/)
+        expect(content).to match(/marsh_grass/)
+        expect(content).to match(/pry/)
+      end
+    end
   end
 end
