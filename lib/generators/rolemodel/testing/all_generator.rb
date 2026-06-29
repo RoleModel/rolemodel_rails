b04dac4b17cbc23015f94ce445ee42b97cfa59b0
Andy Cohen <outlawandy@gmail.com>
2026-06-29 16:30:47 -0500

    use inline: false instead of abort on failure

--- lib/generators/rolemodel/testing/all_generator.rb
+++ lib/generators/rolemodel/testing/all_generator.rb
@@ -6,11 +6,11 @@ module Rolemodel
       class_option :js_runner, type: :boolean, default: false, desc: 'Include jasmine-playwright-runner for browser testing'
 
       def run_all_the_generators
-        generate 'rolemodel:testing:factory_bot', abort_on_failure: true
-        generate('rolemodel:testing:jasmine_playwright', abort_on_failure: true) if options.js_runner?
-        generate 'rolemodel:testing:parallel_tests', abort_on_failure: true
-        generate 'rolemodel:testing:rspec', abort_on_failure: true
-        generate 'rolemodel:testing:test_prof', abort_on_failure: true
+        generate 'rolemodel:testing:factory_bot', [inline: false]
+        generate('rolemodel:testing:jasmine_playwright', [inline: false]) if options.js_runner?
+        generate 'rolemodel:testing:parallel_tests', [inline: false]
+        generate 'rolemodel:testing:rspec', [inline: false]
+        generate 'rolemodel:testing:test_prof', [inline: false]
       end
     end
   end
