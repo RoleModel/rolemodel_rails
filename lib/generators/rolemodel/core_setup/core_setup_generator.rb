20776fccd3ea7d4e91e2e986505c555ac04f7fb4
Mark Kraemer <megamarker@gmail.com>
2026-07-06 11:57:20 -0400

    Clean up which items are generated in the core workflow

--- lib/generators/rolemodel/core_setup/core_setup_generator.rb
+++ lib/generators/rolemodel/core_setup/core_setup_generator.rb
@@ -1,7 +1,5 @@
 module Rolemodel
   class CoreSetupGenerator < ::Rolemodel::GeneratorBase
-    class_option :js_runner, type: :boolean, default: false, desc: 'Include jasmine-playwright-runner for browser testing'
-
     def run_the_core_generators
       generate 'rolemodel:github'
       generate 'rolemodel:heroku'
@@ -10,11 +8,11 @@ module Rolemodel
       generate 'rolemodel:sentry'
       generate 'rolemodel:slim'
       generate 'rolemodel:optics:all'
-      generate 'rolemodel:testing:all', options.js_runner? ? '--js-runner' : '--no-js-runner'
+      generate 'rolemodel:testing:all'
       generate 'rolemodel:simple_form'
       generate 'rolemodel:linters:all'
-      generate 'rolemodel:ui_components:all'
-      generate 'rolemodel:editors'
+      generate 'rolemodel:ui_components:flash'
+      generate 'rolemodel:ui_components:modals'
       generate 'rolemodel:lograge'
     end
   end
