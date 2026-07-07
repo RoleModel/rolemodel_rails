9917f95261a2070f324265592b8c0dca1515ee76
Andy Cohen <outlawandy@gmail.com>
2026-07-06 19:08:08 -0500

    remove excessive code documentation in favor of README docs

--- lib/rolemodel/utility/task_tools.rb
+++ lib/rolemodel/utility/task_tools.rb
@@ -34,50 +34,17 @@ module Rolemodel
       end
 
       ##
-      # Enables a pattern for dry-run mode in tasks. Assumes usage of of other utility methods
+      # Enables a pattern for dry-run mode in tasks. Assumes usage of other utility methods
       # like `say_with_time` and `indicate_progress` that provide user feedback, separate from the actions they perform.
       #
       # Tasks can be updated to support this mode by adding `unless dry_run?` to lines of code that write to the file system or database.
-      #
-      # e.g.
-      #   namespace :clear do
-      #     desc 'Delete all non-current report records'
-      #     task expired: :total do
-      #       say_with_time "Detecting & Deleting Expired Reports among #{@total} Total" do
-      #         deleted_reports = 0
-      #         GeneratedReport.find_each.with_index do |report, index|
-      #           indicate_progress(index, @total)
-      #           next if report.current?
-      #
-      #           report.destroy unless dry_run?
-      #           deleted_reports += 1
-      #         end
-      #         say "#{deleted_reports}/#{@total} Records Deleted"
-      #       end
-      #     end
-      #   end
       def dry_run?
         ENV['DRY_RUN'].present?
       end
 
       ##
       # Improves the usability of Rake::Task arguments which are positional and don't support default values.
-      # This method allows invocation with skipped arguments via `_` and supports default values for any missing arguments.
-      #
-      # e.g.
-      #   namespace :users do
-      #     desc 'Seed a dev user with the given name and email'
-      #     task :dev, %i[name email] => :environment do |_, args|
-      #       name, email = sanitize_arguments(args, name: 'RoleModel', email: 'it-support@rolemodelsoftware.com').values_at(:name, :email)
-      #       say_with_time "Seeding Dev User (name: #{name}, email: #{email}, role: Admin)" do
-      #         User.find_or_create(name:, email:, role: 'admin') unless dry_run?
-      #       end
-      #     end
-      #   end
-      #
-      # $ DRY_RUN=true rake users:dev[_,outlawandy@gmail.com]
-      #   -- Seeding Dev User (name: RoleModel, email: outlawandy@gmail.com, role: Admin)
-      #     -> 0.0000s
+      # This method allows invocation with skipped arguments via `_` and supports a defaults hash.
       def sanitize_arguments(args, defaults = {})
         defaults.merge(ArgumentSanitizer.new(args).to_h)
       end
