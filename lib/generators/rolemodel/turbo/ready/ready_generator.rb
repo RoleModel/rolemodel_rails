10c6255e19081ebbf240c02dafefbc418d2379a5
Andy Cohen <outlawandy@gmail.com>
2026-07-21 09:48:49 -0500

    updates to rolemodel:turbo:ready

--- lib/generators/rolemodel/turbo/ready/ready_generator.rb
+++ lib/generators/rolemodel/turbo/ready/ready_generator.rb
@@ -3,15 +3,22 @@ module Rolemodel
     class ReadyGenerator < ::Rolemodel::GeneratorBase
       source_root File.expand_path('templates', __dir__)
 
-      def javascript_entrypoint
-        say 'generating & importing javascript files', :green
+      def add_javascript_initializer
+        say 'generating & importing morph event handlers', :green
 
-        directory 'app/javascript/controllers'
         directory 'app/javascript/initializers'
 
         append_to_file 'app/javascript/application.js', <<~JS
           import './initializers/before_morph_handler.js'
         JS
+      end
+
+      def add_stimulus_controller
+        say 'adding prevent-morph controller & updating manifest', :green
+
+        directory 'app/javascript/controllers'
+
+        rails_command 'stimulus:manifest:update'
       end
 
       def inject_meta_tags
@@ -28,18 +35,12 @@ module Rolemodel
       def inject_head_outlet
         say 'adding head outlet to head partial', :green
 
-        inject_into_file 'app/views/application/_head.html.slim', after: /javascript_include_tag.*\n/ do
+        append_to_file 'app/views/application/_head.html.slim' do
           optimize_indentation <<~SLIM
 
             = yield :head
           SLIM
         end
-      end
-
-      def register_stimulus_controller
-        say 'updating stimulus manifest', :green
-
-        rails_command 'stimulus:manifest:update'
       end
     end
   end
